f2c1bd2d3e3691673ed5ad8ab933a20078855df7
Anya Bu <annabukhtiiarova@gmail.com>
2021-11-30 16:05:57 +0200

    js code done

---
+++ index.js
@@ -0,0 +1,6 @@
+const first_num = prompt('Enter first number, please');
+const second_num = prompt('Enter second number, please');
+
+const result = Number(first_num) + Number(second_num);
+
+alert(`${first_num} + ${second_num} = ${result}`);
